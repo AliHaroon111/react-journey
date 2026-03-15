db18aca311c498e958c0801726ba9635492fb353
Ali Haroon <fa23-bcs-243@students.cuisahiwal.edu.pk>
2026-03-15 13:18:21 +0500

    Exploring how to create own react library Succesfully Implemented click me to visit google

--- 02CustomReact/customreact.js
+++ 02CustomReact/customreact.js
@@ -1,12 +1,20 @@
-function mainContianer(reactElement,container){
-    const domElement = document.createElement(reactElement.type)// is slct element which avail in reactElement
+function customRender(reactElement,container){
+   /* const domElement = document.createElement(reactElement.type)// is slct element which avail in reactElement
     domElement.innerHTML = reactElement.children
     // Now we have to set attribute
     domElement.setAttriute('href', reactElement.props.href)
     domElement.setAttriute('target',reactElement.props.target)
     //now i need to utilise the container that above passed
-    container.appendChild(domElement)
-}
+    container.appendChild(domElement)                           // this is not React basic JS revision
+    */
+   const domElement = document.createElement(reactElement.type)
+   domElement.innerHTML = reactElement.children
+   for (const prop in reactElement.props) {
+       if(prop === 'children') continue
+      domElement.setAttribute(prop, reactElement.props[prop])
+   }
+   container.appendChild(domElement)
+    }
 
 const reactElement={
     type : 'a',
